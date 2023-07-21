353f73589594f5d53cc8ed7882b9dec01bb027d7
liamjken <liamjken@gmail.com>
2023-07-20 21:32:50 -0400

    push

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@
  */
 
 function enqueue_vue_scripts() {
-    wp_enqueue_script( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-97a17125.js', array(), '1.1', true );
-    wp_enqueue_style( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-683d4dfe.css', array(), '1.1' );
+    wp_enqueue_script( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-94e77084.js', array(), '1.1', true );
+    wp_enqueue_style( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-3822d8a5.css', array(), '1.1' );
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_vue_scripts' );
