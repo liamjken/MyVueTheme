9c41bd07b9975a3c4826729bfa970561821eb28b
liamjken <liamjken@gmail.com>
2023-07-24 15:33:00 -0400

    added 404 page and linked up privacy

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@
  */
 
 function enqueue_vue_scripts() {
-    wp_enqueue_script( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-1cd05537.js', array(), '1.5', true );
-    wp_enqueue_style( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-e1d8d0c3.css', array(), '1.5' );
+    wp_enqueue_script( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-1822be1f.js', array(), '1.6', true );
+    wp_enqueue_style( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-e1d8d0c3.css', array(), '1.6' );
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_vue_scripts' );
