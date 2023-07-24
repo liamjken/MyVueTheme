b93190c69d143478b1ebabab49b6bba09ccccdb3
liamjken <liamjken@gmail.com>
2023-07-24 15:40:10 -0400

    fixing

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@
  */
 
 function enqueue_vue_scripts() {
-    wp_enqueue_script( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-1822be1f.js', array(), '1.6', true );
-    wp_enqueue_style( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-8e7f9d59.css', array(), '1.6' );
+    wp_enqueue_script( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-8e7f9d59.js', array(), '1.6', true );
+    wp_enqueue_style( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-e1d8d0c3.css', array(), '1.6' );
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_vue_scripts' );
