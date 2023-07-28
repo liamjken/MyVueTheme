c80ba82feb89390ed88e7a6528272593abac3bc9
liamjken <liamjken@gmail.com>
2023-07-28 11:52:45 -0400

    demo rotate page

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@
  */
 
 function enqueue_vue_scripts() {
-    wp_enqueue_script( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-8e7f9d59.js', array(), '1.6', true );
-    wp_enqueue_style( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-e1d8d0c3.css', array(), '1.6' );
+    wp_enqueue_script( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-b4485de2.js', array(), '1.7', true );
+    wp_enqueue_style( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-2a904930.css', array(), '1.7' );
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_vue_scripts' );
