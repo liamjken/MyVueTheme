fbb6247b7c6950aefcb618b0494aa4096f13bd68
liamjken <liamjken@gmail.com>
2023-08-30 11:49:48 -0400

    update

--- functions.php
+++ functions.php
@@ -5,27 +5,7 @@
  * @package MyVueTheme
  */
 
- if ( ! function_exists( 'myvuetheme_support' ) ) :
-
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
-	 */
-	function myvuetheme_support() {
-
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
-        add_theme_support( 'custom-spacing' );
-
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
-
-	}
-
-endif;
+ add_theme_support( 'wp-block-styles' ); 
 
 add_action( 'after_setup_theme', 'myvuetheme_support' );
 
