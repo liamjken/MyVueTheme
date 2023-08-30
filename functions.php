3c174537edf36ed4bc3de0bfbc3e386bfc74544a
liamjken <liamjken@gmail.com>
2023-08-29 20:34:44 -0400

    update

--- functions.php
+++ functions.php
@@ -5,6 +5,20 @@
  * @package MyVueTheme
  */
 
+ function myVueThemeSupports_support() {
+
+    // Add support for block styles.
+    add_theme_support( 'wp-block-styles' );
+
+    // Enqueue editor styles.
+    add_editor_style( 'style.css' );
+
+}
+
+
+add_action( 'after_setup_theme', 'myVueThemeSupports_support' );
+
+
 function enqueue_vue_scripts() {
     wp_enqueue_script( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-8f2e947b.js', array(), '1.8', true );
     wp_enqueue_style( 'vue-app', get_stylesheet_directory_uri() . '/assets/index-2a904930.css', array(), '1.7' );
