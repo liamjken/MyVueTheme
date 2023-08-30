5a070ebc5ce213012313b8405ffca33e9a022f64
liamjken <liamjken@gmail.com>
2023-08-30 11:56:37 -0400

    update

--- functions.php
+++ functions.php
@@ -14,20 +14,7 @@
 	 *
 	 * @return void
 	 */
-	function myvuetheme_support() {
-
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
-        	add_theme_support( 'custom-spacing' );
-
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
-
-	}
-
-endif;
-
-add_action( 'after_setup_theme', 'myvuetheme_support' );
+add_theme_support( 'wp-block-styles' );
 
 
 function enqueue_vue_scripts() {
